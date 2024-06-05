cef56828a223faaf215f1a8140fdfc187db6c942
Bryan Taboada <b.taboada123@gmail.com>
2024-06-04 20:35:29 -0600

    working on api url

--- assets/js/java.js
+++ assets/js/java.js
@@ -1,15 +1,52 @@
-function apiFetch (event){
-    // event.preventDefault();
-    event = console.log('on click only1')
-// const searchCity = document.getElementById('search').value();
-console.log('on click only ')
+const fetchButton = document.getElementById('searchBtn');
 
-// let testing = "hello"
-// document.getElementById('test').innerHTML = testing;
-// make a function that listens for search and fetches info from api
+
+function apiFetch (){
+    // obtained this code at url: https://openweathermap.org/api/weather-map-2
+    // example call: api.openweathermap.org/data/2.5/forecast?q=London,us&mode=xml&appid={API key}
+const requestUrl = 'http://maps.openweathermap.org/maps/2.0/weather/{op}/{z}/{x}/{y}?appid=a3daa86f12fe3680bfa21c25ee469546';
+
 
 
 }
 
 
-document.getElementById('search').addEventListener('click', apiFetch());
+fetchButton.addEventListener('click', apiFetch());
+
+
+
+// // not my code below
+// const tableBody = document.getElementById('repo-table');
+
+// function getApi() {
+
+//   // fetch request gets a list of all the repos for the node.js organization
+//   const requestUrl = 'https://api.github.com/orgs/nodejs/repos';
+
+//   fetch(requestUrl)
+//     .then(function (response) {
+//       return response.json(); // this is a json string
+//     })
+//     .then(function (data) {
+//       console.log(data); // this is now an array of objects
+//       // ? We use a `for...of` loop here because it's a little less code than a traditional `for` loop. We also don't need to keep track of the index `(i)`.
+//       for (const repo of data) { 
+//         // Creating elements, tablerow, tabledata, and anchor
+//         const createTableRow = document.createElement('tr');
+//         const tableData = document.createElement('td');
+//         const link = document.createElement('a');
+
+//         // Setting the text of link and the href of the link
+//         link.textContent = repo.html_url;
+//         link.href = repo.html_url;
+
+//         // Appending the link to the tabledata and then appending the tabledata to the tablerow
+//         // The tablerow then gets appended to the tablebody
+//         tableData.appendChild(link);
+//         createTableRow.appendChild(tableData);
+//         tableBody.appendChild(createTableRow);
+//       }
+//     });
+// }
+
+// fetchButton.addEventListener('click', getApi);
