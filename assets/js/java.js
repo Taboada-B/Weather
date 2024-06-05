1cfb0647084ff27dbcf4373cb60445b606b63e27
Bryan Taboada <b.taboada123@gmail.com>
2024-06-04 20:13:21 -0600

    adding more html

---
+++ assets/js/java.js
@@ -0,0 +1,15 @@
+function apiFetch (event){
+    // event.preventDefault();
+    event = console.log('on click only1')
+// const searchCity = document.getElementById('search').value();
+console.log('on click only ')
+
+// let testing = "hello"
+// document.getElementById('test').innerHTML = testing;
+// make a function that listens for search and fetches info from api
+
+
+}
+
+
+document.getElementById('search').addEventListener('click', apiFetch());
